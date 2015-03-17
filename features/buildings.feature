f6576d177b4ec086eb072da5ba0b5f726deb27c8
Kt <katie.brown@bt.com>
2015-03-17 12:05:21 +0000

    Add layout and bootstrap. Still looks like written in notepad

--- features/buildings.feature
+++ features/buildings.feature
@@ -4,6 +4,11 @@ Feature: Accessing the service via a web page
        Then I should see the lookup dialogue
         And I should see the view buildings link
 
+  Scenario: Using bootstrap
+      When I visit the lookup service 
+      Then I should see a page that is laid out properly 
+       And not looking like I drew it with a crayon
+
   Scenario: Viewing a list of buildings
       Given There are buildings in the database
        When I go to the buildings page
--- features/steps/access_steps.rb
+++ features/steps/access_steps.rb
@@ -3,15 +3,22 @@ When "I visit the lookup service" do
 end
 
 Then "I should see 'Hello'" do
-  expect(page.body).to have_content('Hello')
+  page.has_content?('Hello')
 end
 
+Then "I should see a page that is laid out properly" do
+  page.has_content?('No llamas were harmed in the making of this website')
+end
+
+And "not looking like I drew it with a crayon" do
+  page.has_content?('jquery')
+end
 
 Then "I should see the lookup dialogue" do
-  expect(page.body).to have_content('Enter building name')
-  expect(page.body).to have_selector('input[type=submit]')
+  page.has_content?('Enter building name')
+  page.has_selector?('input[type=submit]')
 end
 
 And "I should see the view buildings link" do
-  expect(page.body).to have_link('View buildings')
+  page.has_link?('View buildings')
 end
